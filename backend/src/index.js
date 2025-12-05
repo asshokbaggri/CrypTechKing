ecbf2a62592d96b2d05f455a9b153312fb5535f4
asshokbaggri <asshokbaggri@gmail.com>
2025-12-06 00:02:55 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,16 @@
+// backend/src/index.js
 
+import express from "express";
+import WhaleListener from "./services/whale/whale.listener.js";
+import whaleAPI from "./api/whales.api.js";
+
+const app = express();
+
+app.use("/api/whales", whaleAPI);
+
+app.listen(5000, () => {
+    console.log("Backend running on port 5000");
+});
+
+// START WHALE ENGINE
+WhaleListener.start();
