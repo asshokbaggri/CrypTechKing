d5d3656f6144fcc7bbaa25a2da18328e30fe6300
asshokbaggri <asshokbaggri@gmail.com>
2025-12-06 00:55:44 +0530

    Update db.js

--- backend/src/database/db.js
+++ backend/src/database/db.js
@@ -1,1 +1,10 @@
+import { MongoClient } from "mongodb";
+import config from "../config/env.js";
 
+const client = new MongoClient(config.mongoUrl);
+
+await client.connect();
+
+const db = client.db("cryptechking");
+
+export default db;
