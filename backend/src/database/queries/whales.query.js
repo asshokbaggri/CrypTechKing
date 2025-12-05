ee5bf951ebf7052114fa99a7f48976212e090632
asshokbaggri <asshokbaggri@gmail.com>
2025-12-06 00:01:46 +0530

    Update whales.query.js

--- backend/src/database/queries/whales.query.js
+++ backend/src/database/queries/whales.query.js
@@ -1,1 +1,16 @@
+// backend/src/database/queries/whales.query.js
 
+import db from "../db.js";
+
+export const saveWhaleTx = async (data) => {
+    try {
+        await db.collection("whaleTx").insertOne(data);
+    } catch (err) {
+        console.log("DB Error:", err);
+    }
+};
+
+export const getLiveWhales = async () => {
+    return await db.collection("whaleTx")
+        .find().sort({ timestamp: -1 }).limit(100).toArray();
+};
