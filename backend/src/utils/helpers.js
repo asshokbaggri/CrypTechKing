1dfba6245bfd3334e531a4df9a5583c272efa293
asshokbaggri <asshokbaggri@gmail.com>
2025-12-06 00:51:33 +0530

    Update helpers.js

--- backend/src/utils/helpers.js
+++ backend/src/utils/helpers.js
@@ -1,1 +1,9 @@
+export const sleep = (ms) => new Promise(res => setTimeout(res, ms));
 
+export const formatUSD = (num) => {
+    try {
+        return "$" + Number(num).toLocaleString();
+    } catch {
+        return "$0";
+    }
+};
