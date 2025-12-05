9171af7c5991657532936b46214688a28ed1d341
asshokbaggri <asshokbaggri@gmail.com>
2025-12-06 00:52:23 +0530

    Update websocket.js

--- backend/src/utils/websocket.js
+++ backend/src/utils/websocket.js
@@ -1,1 +1,14 @@
+import { WebSocketServer } from "ws";
 
+const wss = new WebSocketServer({ port: 6001 });
+
+wss.on("connection", (ws) => {
+    ws.send(JSON.stringify({ message: "Connected to CrypTechKing WS" }));
+});
+
+export const broadcast = (data) => {
+    const msg = JSON.stringify(data);
+    wss.clients.forEach(client => client.send(msg));
+};
+
+export default wss;
