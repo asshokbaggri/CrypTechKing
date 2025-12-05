abb0e8e21cf7f6c0df26c455f255db6ef0812b87
asshokbaggri <asshokbaggri@gmail.com>
2025-12-06 00:09:44 +0530

    Update pump.api.js

--- backend/src/api/pump.api.js
+++ backend/src/api/pump.api.js
@@ -5,9 +5,18 @@ import { getTrendingPumps } from "../database/queries/pump.query.js";
 
 const router = express.Router();
 
+// TRENDING PUMPS
 router.get("/trending", async (req, res) => {
     const data = await getTrendingPumps();
-    res.json({ trending: data });
+    res.json({
+        status: "success",
+        count: data.length,
+        trending: data
+    });
+});
+
+router.get("/", (req, res) => {
+    res.json({ message: "Pump API working" });
 });
 
 export default router;
