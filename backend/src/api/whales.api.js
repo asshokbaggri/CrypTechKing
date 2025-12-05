a7972af68b10c5cf60e63a9c67f5d2eb5d67d0e6
asshokbaggri <asshokbaggri@gmail.com>
2025-12-06 00:09:02 +0530

    Update whales.api.js

--- backend/src/api/whales.api.js
+++ backend/src/api/whales.api.js
@@ -5,9 +5,19 @@ import { getLiveWhales } from "../database/queries/whales.query.js";
 
 const router = express.Router();
 
+// LIVE FEED
 router.get("/live", async (req, res) => {
     const data = await getLiveWhales();
-    res.json({ whales: data });
+    res.json({
+        status: "success",
+        count: data.length,
+        whales: data
+    });
+});
+
+// HEALTH CHECK
+router.get("/", (req, res) => {
+    res.json({ message: "Whale API working" });
 });
 
 export default router;
