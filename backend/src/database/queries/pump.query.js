5fce6280b6067d3b277380e969d73f2d3e0e7260
asshokbaggri <asshokbaggri@gmail.com>
2025-12-06 00:06:05 +0530

    Update pump.query.js

--- backend/src/database/queries/pump.query.js
+++ backend/src/database/queries/pump.query.js
@@ -1,1 +1,19 @@
+// backend/src/database/queries/pump.query.js
 
+import db from "../db.js";
+
+export const savePumpTrend = async (data) => {
+    try {
+        await db.collection("pumpTrends").insertOne(data);
+    } catch (err) {
+        console.log("Pump DB Error:", err);
+    }
+};
+
+export const getTrendingPumps = async () => {
+    return await db.collection("pumpTrends")
+        .find()
+        .sort({ hypeScore: -1, timestamp: -1 })
+        .limit(50)
+        .toArray();
+};
