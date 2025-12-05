70d4d55e613d862db56519e9c0e3684e07885828
asshokbaggri <asshokbaggri@gmail.com>
2025-12-06 00:56:10 +0530

    Update TokenMeta.model.js

--- backend/src/database/models/TokenMeta.model.js
+++ backend/src/database/models/TokenMeta.model.js
@@ -1,1 +1,10 @@
-
+export default {
+    address: String,
+    symbol: String,
+    chain: String,
+    price: Number,
+    marketCap: Number,
+    holders: Number,
+    liquidity: Number,
+    updatedAt: Number
+};
