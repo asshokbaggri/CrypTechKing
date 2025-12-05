472ebba066e7623adebcf0a03efc0f53b1efe03a
asshokbaggri <asshokbaggri@gmail.com>
2025-12-06 00:06:36 +0530

    Update pump.api.js

--- backend/src/api/pump.api.js
+++ backend/src/api/pump.api.js
@@ -1,1 +1,13 @@
+// backend/src/api/pump.api.js
 
+import express from "express";
+import { getTrendingPumps } from "../database/queries/pump.query.js";
+
+const router = express.Router();
+
+router.get("/trending", async (req, res) => {
+    const data = await getTrendingPumps();
+    res.json({ trending: data });
+});
+
+export default router;
