8caa87b240a91000241d3bcb02aa25e98b95e573
asshokbaggri <asshokbaggri@gmail.com>
2025-12-06 00:56:46 +0530

    Update token.query.js

--- backend/src/database/queries/token.query.js
+++ backend/src/database/queries/token.query.js
@@ -1,1 +1,13 @@
+import db from "../db.js";
 
+export const saveTokenMeta = async (meta) => {
+    await db.collection("tokenMeta").updateOne(
+        { address: meta.address },
+        { $set: meta },
+        { upsert: true }
+    );
+};
+
+export const getTokenMeta = async (address) => {
+    return await db.collection("tokenMeta").findOne({ address });
+};
