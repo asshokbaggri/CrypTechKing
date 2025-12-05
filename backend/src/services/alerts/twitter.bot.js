bc1986ef40b2b5df27deeb2368dc1ce54ba41d77
asshokbaggri <asshokbaggri@gmail.com>
2025-12-06 00:54:12 +0530

    Update twitter.bot.js

--- backend/src/services/alerts/twitter.bot.js
+++ backend/src/services/alerts/twitter.bot.js
@@ -1,1 +1,24 @@
+import { TwitterApi } from "twitter-api-v2";
 
+const client = new TwitterApi({
+    appKey: process.env.TWITTER_API_KEY,
+    appSecret: process.env.TWITTER_API_SECRET,
+    accessToken: process.env.TWITTER_ACCESS_TOKEN,
+    accessSecret: process.env.TWITTER_ACCESS_SECRET
+});
+
+export async function tweetWhale(tx) {
+    const msg = `🐋 Whale Alert  
+$${tx.usdValue.toLocaleString()} moved  
+${tx.fromLabel} → ${tx.toLabel}`;
+
+    await client.v2.tweet(msg);
+}
+
+export async function tweetPump(data) {
+    const msg = `🚀 Pump Alert  
+Token: ${data.symbol}  
+Hype Score: ${data.hypeScore}`;
+
+    await client.v2.tweet(msg);
+}
