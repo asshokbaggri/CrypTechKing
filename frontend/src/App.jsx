ef26bf0b9e5420f72ba7c1a8a1dc2b302e57f688
asshokbaggri <asshokbaggri@gmail.com>
2025-12-06 23:16:56 +0530

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/index.jsx";
+import Pump from "./pages/pump.jsx";
+import Whales from "./pages/whales.jsx";
+import Coin from "./pages/coin.jsx";
+import "../styles/main.css";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pump" element={<Pump />} />
+        <Route path="/whales" element={<Whales />} />
+        <Route path="/coin/:address" element={<Coin />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
