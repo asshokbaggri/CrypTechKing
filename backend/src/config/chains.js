ea538fb34b726afd02b07a055eceac8e4bef6a84
asshokbaggri <asshokbaggri@gmail.com>
2025-12-06 23:53:28 +0530

    Update chains.js

--- backend/src/config/chains.js
+++ backend/src/config/chains.js
@@ -2,6 +2,12 @@
 
 export default {
     ETH: {
-        HTTP: process.env.ALCHEMY_ETH_HTTP
+        WS: "wss://eth-mainnet.g.alchemy.com/v2/XXXX"
+    },
+    BNB: {
+        WS: "wss://bsc-mainnet.nodereal.io/ws/v1/XXXX"
+    },
+    POLYGON: {
+        WS: "wss://polygon-mainnet.g.alchemy.com/v2/XXXX"
     }
 };
