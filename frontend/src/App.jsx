c51aca31c26605538ec0546ff4a00d57abac111f
asshokbaggri <asshokbaggri@gmail.com>
2025-12-07 22:46:06 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/index.jsx";
-import Pump from "./pages/pump.jsx";
-import Whales from "./pages/whales.jsx";
-import Coin from "./pages/coin.jsx";
-import "./styles/main.css";
+import DashboardLayout from "./layout/DashboardLayout";
+
+import Dashboard from "./pages/Dashboard";
+import Whales from "./pages/Whales";
+import SmartMoney from "./pages/SmartMoney";
+import Pump from "./pages/Pump";
+import Alerts from "./pages/Alerts";
+import TokenPage from "./pages/TokenPage";
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/pump" element={<Pump />} />
-        <Route path="/whales" element={<Whales />} />
-        <Route path="/coin/:address" element={<Coin />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <DashboardLayout>
+                <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/whales" element={<Whales />} />
+                    <Route path="/smartmoney" element={<SmartMoney />} />
+                    <Route path="/pump" element={<Pump />} />
+                    <Route path="/alerts" element={<Alerts />} />
+                    <Route path="/token/:address" element={<TokenPage />} />
+                </Routes>
+            </DashboardLayout>
+        </BrowserRouter>
+    );
 }
