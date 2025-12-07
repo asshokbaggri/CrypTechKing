1c2847a833955c469c50f592b6ae4f12d774063a
asshokbaggri <asshokbaggri@gmail.com>
2025-12-07 13:27:27 +0530

    Update env.js

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -2,14 +2,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default {
-    mongoUrl: process.env.MONGO_URL || "",
+    MONGO_URL: process.env.MONGO_URL,
 
-    ETH_HTTP: process.env.ALCHEMY_ETH_HTTP || "",
-    ETH_WS: process.env.ALCHEMY_ETH_WS || "",
+    ALCHEMY_ETH_HTTP: process.env.ALCHEMY_ETH_HTTP,
+    ALCHEMY_ETH_WS: process.env.ALCHEMY_ETH_WS,
 
-    POLYGON_HTTP: process.env.ALCHEMY_POLYGON_HTTP || "",
-    POLYGON_WS: process.env.ALCHEMY_POLYGON_WS || "",
+    ALCHEMY_POLYGON_HTTP: process.env.ALCHEMY_POLYGON_HTTP,
+    ALCHEMY_POLYGON_WS: process.env.ALCHEMY_POLYGON_WS,
 
-    BNB_HTTP: process.env.BNB_HTTP || "",
-    BNB_WS: process.env.BNB_WS || ""
+    BNB_HTTP: process.env.BNB_HTTP,
+    BNB_WS: process.env.BNB_WS
 };
