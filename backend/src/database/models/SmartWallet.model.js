518a2564a53adbbad97cfa38a2928ea6ed793b12
asshokbaggri <asshokbaggri@gmail.com>
2025-12-07 21:28:23 +0530

    Create SmartWallet.model.js

---
+++ backend/src/database/models/SmartWallet.model.js
@@ -0,0 +1,10 @@
+// backend/src/database/models/SmartWallet.model.js
+
+export default {
+    wallet: String,
+    score: Number,
+    labels: Array,
+    totalVolume: Number,
+    winRate: Number,
+    lastActive: Number
+};
