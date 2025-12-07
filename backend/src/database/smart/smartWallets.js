19ee197b33dc2b180be3b7343511985f31c8e18c
asshokbaggri <asshokbaggri@gmail.com>
2025-12-07 21:02:16 +0530

    Create smartWallets.js

---
+++ backend/src/database/smart/smartWallets.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        address: "0x1234...",
+        chain: "ETH",
+        tags: ["early-buyer", "sniper", "profit-king"]
+    },
+    {
+        address: "0xabcd...",
+        chain: "BNB",
+        tags: ["bnb-whale", "ai-trader"]
+    },
+    {
+        address: "0x5678...",
+        chain: "POLYGON",
+        tags: ["matic-shark"]
+    }
+];
