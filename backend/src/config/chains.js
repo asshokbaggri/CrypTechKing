f0889158a08cf8a973e62ba3951420c73eb0caed
asshokbaggri <asshokbaggri@gmail.com>
2025-12-07 13:28:01 +0530

    Update chains.js

--- backend/src/config/chains.js
+++ backend/src/config/chains.js
@@ -2,18 +2,15 @@ import ENV from "./env.js";
 
 export default {
     ETH: {
-        NAME: "Ethereum",
-        HTTP: ENV.ETH_HTTP,
-        WS: ENV.ETH_WS
+        HTTP: ENV.ALCHEMY_ETH_HTTP || "",
+        WS: ENV.ALCHEMY_ETH_WS || ""
     },
     POLYGON: {
-        NAME: "Polygon",
-        HTTP: ENV.POLYGON_HTTP,
-        WS: ENV.POLYGON_WS
+        HTTP: ENV.ALCHEMY_POLYGON_HTTP || "",
+        WS: ENV.ALCHEMY_POLYGON_WS || ""
     },
     BNB: {
-        NAME: "BNB Chain",
-        HTTP: ENV.BNB_HTTP,
-        WS: ENV.BNB_WS
+        HTTP: ENV.BNB_HTTP || "",
+        WS: ENV.BNB_WS || ""
     }
 };
