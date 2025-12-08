e629398682f4912ec2ec5708710a8f23d8b6c3b5
asshokbaggri <asshokbaggri@gmail.com>
2025-12-08 16:01:11 +0530

    Create format.js

---
+++ frontend/src/utils/format.js
@@ -0,0 +1,12 @@
+export default function format(number) {
+    if (!number || isNaN(number)) return "0";
+
+    number = Number(number);
+
+    if (number >= 1e12) return (number / 1e12).toFixed(2) + "T";
+    if (number >= 1e9) return (number / 1e9).toFixed(2) + "B";
+    if (number >= 1e6) return (number / 1e6).toFixed(2) + "M";
+    if (number >= 1e3) return (number / 1e3).toFixed(2) + "K";
+
+    return number.toString();
+}
