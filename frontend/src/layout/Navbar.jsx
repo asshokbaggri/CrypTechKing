43bafc9b03918a4b2200c6df2cf8926efdac58f5
asshokbaggri <asshokbaggri@gmail.com>
2025-12-09 12:09:08 +0530

    Create Navbar.jsx

---
+++ frontend/src/layout/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+    return (
+        <div className="navbar">
+            <h2>CrypTechKing Dashboard</h2>
+
+            <div className="right">
+                <span className="status live">● Live</span>
+            </div>
+        </div>
+    );
+}
