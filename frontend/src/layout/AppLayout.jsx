5cad8746749cf85db23aea35ada248250b6be303
asshokbaggri <asshokbaggri@gmail.com>
2025-12-09 13:17:02 +0530

    Update AppLayout.jsx

--- frontend/src/layout/AppLayout.jsx
+++ frontend/src/layout/AppLayout.jsx
@@ -1,17 +1,31 @@
-import Sidebar from "./Sidebar";
-import Navbar from "./Navbar";
-
 export default function AppLayout({ children }) {
-    return (
-        <div className="app-container">
-            <Sidebar />
+  return (
+    <div style={{ display: "flex" }}>
+      {/* LEFT SIDEBAR */}
+      <div style={{
+        width: "240px",
+        background: "#0d0d12",
+        height: "100vh",
+        padding: "20px",
+        color: "#fff",
+        position: "fixed",
+        left: 0,
+        top: 0,
+      }}>
+        <h2>CrypTechKing</h2>
+        <ul style={{ marginTop: "30px", listStyle: "none", padding: 0 }}>
+          <li><a href="/dashboard">Dashboard</a></li>
+          <li><a href="/whales">Whales</a></li>
+          <li><a href="/smartmoney">Smart Money</a></li>
+          <li><a href="/pump">Pump Scanner</a></li>
+          <li><a href="/alerts">Alerts</a></li>
+        </ul>
+      </div>
 
-            <main className="content-wrapper">
-                <Navbar />
-                <div className="page-content">
-                    {children}
-                </div>
-            </main>
-        </div>
-    );
+      {/* MAIN CONTENT */}
+      <div style={{ marginLeft: "260px", padding: "20px", width: "100%" }}>
+        {children}
+      </div>
+    </div>
+  );
 }
