ad6862ced64a1822f6570e717e58a5729a756348
asshokbaggri <asshokbaggri@gmail.com>
2025-12-09 12:51:20 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
-import Whales from "./pages/Whales";
-import Pump from "./pages/Pump";
-import Token from "./pages/Token";
+
+import Dashboard from "./pages/Dashboard.jsx";
+import Whales from "./pages/Whales.jsx";
+import Pump from "./pages/Pump.jsx";
+import Token from "./pages/Token.jsx";
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/whales" element={<Whales />} />
-        <Route path="/pump" element={<Pump />} />
-        <Route path="/token/:address" element={<Token />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/whales" element={<Whales />} />
+                <Route path="/pump" element={<Pump />} />
+                <Route path="/token/:address" element={<Token />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
