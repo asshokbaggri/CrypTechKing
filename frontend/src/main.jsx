06373d46ff11eca3fb9cd9679b54c76cbd3f27ed
asshokbaggri <asshokbaggri@gmail.com>
2025-12-09 12:45:36 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./styles/main.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Whales from "./pages/Whales";
+import Pump from "./pages/Pump";
+import Token from "./pages/Token";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/whales" element={<Whales />} />
+        <Route path="/pump" element={<Pump />} />
+        <Route path="/token/:address" element={<Token />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
