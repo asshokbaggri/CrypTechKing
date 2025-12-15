7ea263f6da3a29ef6b76385f8774d3172b85a31a
asshokbaggri <asshokbaggri@gmail.com>
2025-12-15 16:12:37 +0530

    Update Token.model.js

--- backend/src/models/Token.model.js
+++ backend/src/models/Token.model.js
@@ -1,1 +1,28 @@
+import mongoose from "mongoose";
 
+const TokenSchema = new mongoose.Schema(
+  {
+    chain: {
+      type: String,
+      index: true
+    },
+
+    address: {
+      type: String,
+      index: true
+    },
+
+    symbol: String,
+    name: String,
+    decimals: Number,
+
+    priceUsd: Number,
+    liquidityUsd: Number,
+    volume24h: Number,
+
+    lastUpdated: Date
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Token", TokenSchema);
