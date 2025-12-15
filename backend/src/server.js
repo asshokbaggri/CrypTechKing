2f10130fcfe1cbb109dc349cc40fc3044064cf53
asshokbaggri <asshokbaggri@gmail.com>
2025-12-15 15:53:34 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,12 @@
+import app from "./app.js";
+import { ENV, connectDB } from "./config/index.js";
 
+const startServer = async () => {
+  await connectDB();
+
+  app.listen(ENV.PORT, () => {
+    console.log(`🚀 Server running on port ${ENV.PORT}`);
+  });
+};
+
+startServer();
