f99e02e06872e1d989ce3835b4c9a2cda855f757
asshokbaggri <asshokbaggri@gmail.com>
2025-12-15 16:29:04 +0530

    Create index.js

---
+++ backend/src/services/index.js
@@ -0,0 +1,4 @@
+export { default as whaleService } from "./whale.service.js";
+export { default as smartMoneyService } from "./smartmoney.service.js";
+export { default as pumpService } from "./pump.service.js";
+export { default as alertService } from "./alert.service.js";
