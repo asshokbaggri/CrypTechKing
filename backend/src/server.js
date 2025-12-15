d5c3d5315dad865e5800568d92c99956928fb7e1
asshokbaggri <asshokbaggri@gmail.com>
2025-12-15 18:48:03 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,16 @@
 import app from "./app.js";
 import { ENV, connectDB } from "./config/index.js";
 
+import { startEthWhaleListener } from "./listeners/ethWhale.listener.js";
+
 const startServer = async () => {
   await connectDB();
 
   app.listen(ENV.PORT, () => {
     console.log(`🚀 Server running on port ${ENV.PORT}`);
+
+    // 🔥 START WHALE LISTENER HERE (CRITICAL)
+    startEthWhaleListener();
   });
 };
 
