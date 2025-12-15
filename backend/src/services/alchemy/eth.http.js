c2d77c7f0cac7dfac050247e9b013ec184ec0196
asshokbaggri <asshokbaggri@gmail.com>
2025-12-15 20:28:04 +0530

    Update eth.http.js

--- backend/src/services/alchemy/eth.http.js
+++ backend/src/services/alchemy/eth.http.js
@@ -1,7 +1,8 @@
-// eth.http.js
 import { Alchemy, Network } from "alchemy-sdk";
 
-export default new Alchemy({
+const alchemy = new Alchemy({
   apiKey: process.env.ALCHEMY_ETH_HTTP,
   network: Network.ETH_MAINNET,
 });
+
+export default alchemy;
