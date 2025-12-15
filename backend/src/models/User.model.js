3d58134745b768fb03aa11dc5a595c1c71c6d54e
asshokbaggri <asshokbaggri@gmail.com>
2025-12-15 16:10:15 +0530

    Update User.model.js

--- backend/src/models/User.model.js
+++ backend/src/models/User.model.js
@@ -1,1 +1,31 @@
+import mongoose from "mongoose";
 
+const UserSchema = new mongoose.Schema(
+  {
+    wallet: {
+      type: String,
+      unique: true,
+      sparse: true,
+      index: true
+    },
+
+    telegramId: {
+      type: String,
+      index: true
+    },
+
+    role: {
+      type: String,
+      default: "user",
+      enum: ["user", "admin"]
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true
+    }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", UserSchema);
