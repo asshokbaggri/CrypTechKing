e433b0a267d6fe7e97c5d3dc60d178de477e1c9c
asshokbaggri <asshokbaggri@gmail.com>
2025-12-15 16:21:27 +0530

    Update whale.routes.js

--- backend/src/routes/whale.routes.js
+++ backend/src/routes/whale.routes.js
@@ -1,1 +1,12 @@
+// backend/src/routes/whale.routes.js
+import express from "express";
+import { whaleController } from "../controllers/index.js";
 
+const router = express.Router();
+
+/**
+ * GET /api/whales/latest
+ */
+router.get("/latest", whaleController.getLatestWhales);
+
+export default router;
