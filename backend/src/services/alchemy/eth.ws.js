2452c3957b8459c869308af2d4d273d935bd6299
asshokbaggri <asshokbaggri@gmail.com>
2025-12-15 20:37:48 +0530

    Update eth.ws.js

--- backend/src/services/alchemy/eth.ws.js
+++ backend/src/services/alchemy/eth.ws.js
@@ -4,4 +4,12 @@ const provider = new WebSocketProvider(
   process.env.ALCHEMY_ETH_WS
 );
 
+provider._websocket.on("error", (err) => {
+  console.error("ETH WS error", err.message);
+});
+
+provider._websocket.on("close", () => {
+  console.warn("ETH WS closed");
+});
+
 export default provider;
