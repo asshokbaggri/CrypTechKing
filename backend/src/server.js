b6399565f76b2279d4524745069337f25d90e573
asshokbaggri <asshokbaggri@gmail.com>
2025-12-15 20:11:57 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,20 @@
 import app from "./app.js";
 import { ENV, connectDB } from "./config/index.js";
 
+// 🐳 Whale listeners
 import { startEthWhaleListener } from "./listeners/ethWhale.listener.js";
+// later:
+// import { startPolygonWhaleListener } from "./listeners/polygonWhale.listener.js";
+// import { startBnbWhaleListener } from "./listeners/bnbWhale.listener.js";
 
 const startServer = async () => {
   await connectDB();
 
+  // 🔥 START LISTENERS AFTER DB IS READY
+  startEthWhaleListener();
+
   app.listen(ENV.PORT, () => {
     console.log(`🚀 Server running on port ${ENV.PORT}`);
-
-    // 🔥 START WHALE LISTENER HERE (CRITICAL)
-    startEthWhaleListener();
   });
 };
 
