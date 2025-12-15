d1deff1bf53951cf8796ba05bfeb1f3b52970791
asshokbaggri <asshokbaggri@gmail.com>
2025-12-15 16:11:54 +0530

    Update WhaleTx.model.js

--- backend/src/models/WhaleTx.model.js
+++ backend/src/models/WhaleTx.model.js
@@ -1,1 +1,34 @@
+import mongoose from "mongoose";
 
+const WhaleTxSchema = new mongoose.Schema(
+  {
+    chain: {
+      type: String,
+      index: true
+    },
+
+    hash: {
+      type: String,
+      unique: true
+    },
+
+    from: String,
+    to: String,
+
+    tokenAddress: {
+      type: String,
+      index: true
+    },
+
+    tokenSymbol: String,
+
+    amount: Number,
+    usdValue: Number,
+
+    blockNumber: Number,
+    timestamp: Date
+  },
+  { timestamps: false }
+);
+
+export default mongoose.model("WhaleTx", WhaleTxSchema);
