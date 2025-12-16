ac7e098c4266fd6790a7163bdcc58a5b156e755b
asshokbaggri <asshokbaggri@gmail.com>
2025-12-16 12:44:00 +0530

    Update env.js

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -1,5 +1,5 @@
 export const ENV = {
-  PORT: process.env.PORT || 3000,
+  PORT: process.env.PORT || 8080,
   MONGO_URL: process.env.MONGO_URL,
   ALCHEMY_WEBHOOK_SECRET: process.env.ALCHEMY_WEBHOOK_SECRET
 };
