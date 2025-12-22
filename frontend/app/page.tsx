3bee711868d6efde83cf251a511d021a3545d601
asshokbaggri <asshokbaggri@gmail.com>
2025-12-23 00:58:42 +0530

    Update page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -16,6 +16,7 @@ export default async function Page() {
       <HalvingStats
         currentBlock={data.currentBlock}
         blocksLeft={data.blocksLeft}
+        secondsLeft={data.secondsLeft}
       />
     </main>
   );
