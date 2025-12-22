f541150f4b474471b2490ec8f0057486a98f6d95
asshokbaggri <asshokbaggri@gmail.com>
2025-12-22 23:56:52 +0530

    Create page.js

---
+++ bitcoin-halving/app/page.js
@@ -0,0 +1,11 @@
+import HalvingCountdown from "@/components/HalvingCountdown";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Bitcoin Halving 2028</h1>
+      <p>The next Bitcoin supply shock event.</p>
+      <HalvingCountdown />
+    </main>
+  );
+}
