948adc19888cff039f2c527cabc034152ed73907
asshokbaggri <asshokbaggri@gmail.com>
2025-12-23 23:58:35 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,10 @@ import 'dotenv/config';
 import cron from 'node-cron';
 import runChaosJob from './jobs/chaos.job.js';
 
+import connectMongo from './config/mongo.js';
+
+await connectMongo();
+
 const PORT = process.env.PORT || 8080;
 
 cron.schedule('*/15 * * * *', async () => {
