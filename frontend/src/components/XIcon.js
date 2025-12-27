99eb0274e3f24e2f8f0a993a2cd2b165a2113699
asshokbaggri <asshokbaggri@gmail.com>
2025-12-28 00:52:04 +0530

    Create XIcon.js

---
+++ frontend/src/components/XIcon.js
@@ -0,0 +1,12 @@
+export default function XIcon({ size = 16 }) {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="currentColor"
+    >
+      <path d="M18.9 2H22l-7.4 8.4L23 22h-6.8l-5.4-7-6.1 7H2l7.9-9L1 2h7l4.9 6.3L18.9 2z" />
+    </svg>
+  )
+}
