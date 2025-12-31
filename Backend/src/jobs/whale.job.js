86e8b9401dddc354d0e440ca2275a96f5079a5ad
asshokbaggri <asshokbaggri@gmail.com>
2025-12-31 21:22:48 +0530

    Create whale.job.js

---
+++ Backend/src/jobs/whale.job.js
@@ -0,0 +1,11 @@
+import sendTelegramMessage from '../services/telegram.service.js'
+import { log } from '../utils/logger.js'
+
+export default function startWhaleJob() {
+  log('Whale job started')
+
+  // Placeholder – logic next step
+  // setInterval(async () => {
+  //   detect whale
+  // }, 15000)
+}
