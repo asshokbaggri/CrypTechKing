23e481562577573c0e2724c032b6dd525664ff2b
asshokbaggri <asshokbaggri@gmail.com>
2025-12-31 18:35:47 +0530

    Create exchangeWallets.js

---
+++ backend/src/data/exchangeWallets.js
@@ -0,0 +1,13 @@
+// backend/src/data/exchangeWallets.js
+
+export const EXCHANGE_WALLETS = {
+  BINANCE: [
+    '0x28c6c06298d514db089934071355e5743bf21d60'
+  ],
+  COINBASE: [
+    '0x503828976d22510aad0201ac7ec88293211d23da'
+  ],
+  KRAKEN: [
+    '0x267be1c1d684f78cb4f6a176c4911b741e4ffdc0'
+  ]
+};
