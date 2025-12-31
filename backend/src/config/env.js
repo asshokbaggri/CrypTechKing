4d9012592f7201687e948a494bd7ea8a1f2de4e3
asshokbaggri <asshokbaggri@gmail.com>
2025-12-31 20:09:16 +0530

    Delete backend directory

--- backend/src/config/env.js
+++
@@ -1,5 +0,0 @@
-export const ENV = {
-  PORT: process.env.PORT || 8080,
-  WHALE_THRESHOLD: Number(process.env.WHALE_THRESHOLD),
-  VOLUME_MULTIPLIER: Number(process.env.VOLUME_MULTIPLIER),
-};
--- backend/src/config/mongo.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-export default async function connectMongo() {
-  if (!process.env.MONGO_URI) return;
-
-  await mongoose.connect(process.env.MONGO_URI);
-  console.log('🟢 MongoDB connected');
-}
